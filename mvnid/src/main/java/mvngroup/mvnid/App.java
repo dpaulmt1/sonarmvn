698b9779901926ce74644415bfba13bbbf7f8c1e
Diego Paul Montalvo Paez <dpmontalvo@todo1.com>
2018-05-09 14:24:17 -0500

    1 commit

---
+++ mvnid/src/main/java/mvngroup/mvnid/App.java
@@ -0,0 +1,13 @@
+package mvngroup.mvnid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello my World!" );
+    }
+}
